6bc9fe5aecf0f371733f4114ba1db7156acb09df
Agustin Jouanny <arj_agustin@gotmail.com>
2022-10-17 21:08:42 -0300

    correcciones de views templates y formularios

--- Proyecto_final/blog/views.py
+++ Proyecto_final/blog/views.py
@@ -8,22 +8,29 @@ from blog.forms import Articuloform, Autorform, Seccionform
 
 
 def inicio(request):
-    return render(request, "/inicio.html")
+    return render(request, "inicio.html")
 
 
 def formulario_articulos(request):
-    mi_formulario: Articuloform()
+    #    if request.method == "GET":
+
+    mi_formulario = Articuloform()
     contexto = {"formulario": mi_formulario}
-    return render(request, "blog/formulario-articulo.html", context=contexto)
+    return render(request, "formulario-articulo.html", context=contexto)
+
+
+#    if request.method == "POST":
+#        mi_formulario = Articuloform(request.POST)
+#        if mi_formulario.is_valid():
 
 
 def formulario_secciones(request):
-    mi_formulario: Seccionform()
+    mi_formulario = Seccionform()
     contexto = {"formulario": mi_formulario}
-    return render(request, "blog/formulario-seccion.html", context=contexto)
+    return render(request, "formulario-seccion.html", context=contexto)
 
 
 def formulario_autores(request):
-    mi_formulario: Autorform()
+    mi_formulario = Autorform()
     contexto = {"formulario": mi_formulario}
-    return render(request, "blog/formulario-autor.html", context=contexto)
+    return render(request, "formulario-autor.html", context=contexto)
