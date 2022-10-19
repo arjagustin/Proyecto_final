1d567daa708f33c1cd7b00a5b3d964b676e3e1db
Agustin Jouanny <arj_agustin@gotmail.com>
2022-10-19 17:04:29 -0300

    guardado en bd de info de formularios

--- Proyecto_final/blog/views.py
+++ Proyecto_final/blog/views.py
@@ -1,8 +1,11 @@
 from multiprocessing import context
+from operator import is_not
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from blog.forms import Articuloform, Autorform, Seccionform
+from blog.models import Articulo, Seccion, Autor
+from datetime import datetime
 
 # Create your views here.
 
@@ -12,25 +15,72 @@ def inicio(request):
 
 
 def formulario_articulos(request):
-    #    if request.method == "GET":
+    if request.method == "GET":
+        mi_formulario = Articuloform()
+        contexto = {"formulario": mi_formulario}
+        return render(request, "formulario-articulo.html", context=contexto)
 
-    mi_formulario = Articuloform()
-    contexto = {"formulario": mi_formulario}
-    return render(request, "formulario-articulo.html", context=contexto)
+    if request.method == "POST":
 
+        mi_formulario = Articuloform(request.POST)
+        if mi_formulario.is_valid():
+            mi_formulario_completado = mi_formulario.cleaned_data
+            nuevo_articulo = Articulo(
+                titulo=mi_formulario_completado["titulo"],
+                articulo=mi_formulario_completado["articulo"],
+                fecha_de_publicacion=mi_formulario_completado["fecha_de_publicacion"],
+            )
+            nuevo_articulo.save()
+            return render(request, "formulario_exitoso.html")
 
-#    if request.method == "POST":
-#        mi_formulario = Articuloform(request.POST)
-#        if mi_formulario.is_valid():
+        contexto = {"formulario": mi_formulario}
+        return render(request, "formulario-articulo.html", context=contexto)
 
 
 def formulario_secciones(request):
-    mi_formulario = Seccionform()
-    contexto = {"formulario": mi_formulario}
-    return render(request, "formulario-seccion.html", context=contexto)
+    if request.method == "GET":
+
+        mi_formulario = Seccionform()
+        contexto = {"formulario": mi_formulario}
+        return render(request, "formulario-seccion.html", context=contexto)
+
+    if request.method == "POST":
+
+        mi_formulario = Seccionform(request.POST)
+        if mi_formulario.is_valid():
+            mi_formulario_completado = mi_formulario.cleaned_data
+            nuevo_articulo = Seccion(
+                nombre=mi_formulario_completado["nombre"],
+                fecha_de_creacion=mi_formulario_completado["fecha_de_creacion"],
+            )
+            nuevo_articulo.save()
+            return render(request, "formulario_exitoso.html")
+
+        contexto = {"formulario": mi_formulario}
+        return render(request, "formulario-seccion.html", context=contexto)
 
 
 def formulario_autores(request):
-    mi_formulario = Autorform()
-    contexto = {"formulario": mi_formulario}
-    return render(request, "formulario-autor.html", context=contexto)
+    if request.method == "GET":
+
+        mi_formulario = Autorform()
+        contexto = {"formulario": mi_formulario}
+        return render(request, "formulario-autor.html", context=contexto)
+
+    if request.method == "POST":
+
+        mi_formulario = Autorform(request.POST)
+        if mi_formulario.is_valid():
+            mi_formulario_completado = mi_formulario.cleaned_data
+            nuevo_articulo = Autor(
+                nombre=mi_formulario_completado["nombre"],
+                apodo=mi_formulario_completado["apodo"],
+                profecion=mi_formulario_completado["profecion"],
+                edad=mi_formulario_completado["edad"],
+                mail=mi_formulario_completado["mail"],
+            )
+            nuevo_articulo.save()
+            return render(request, "formulario_exitoso.html")
+
+        contexto = {"formulario": mi_formulario}
+        return render(request, "formulario-autor.html", context=contexto)
